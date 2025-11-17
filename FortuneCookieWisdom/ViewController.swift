ba30d229367c980b20366cf421efa2dc927d2d47
Fernando Bulnes Garrorena <fernando_bulnesga@mymail.eku.edu>
2025-11-17 16:32:05 -0500

    Initial Commit

---
+++ FortuneCookieWisdom/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FortuneCookieWisdom
+//
+//  Created by Anastasiia Cherniakova on 11/17/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
